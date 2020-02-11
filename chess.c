45f7cca5bc82e428c5aff151792fb58ae4287d5a
Крупенков Александр Евгеньевич <is942s08@awp06.csc.local>
2020-02-11 16:25:53 +0700

    Implement command line argument parsing

--- chess.c
+++ chess.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main (int argc, int* argv[])
+int main (int argc, char* argv[])
 {
+    if (argc != 2) {
+	printf("Usage: ./chess [file]\n");
+	return 0;
+    }
+    
+    char* filename = argv[1];
+    
     return 0;
 }
