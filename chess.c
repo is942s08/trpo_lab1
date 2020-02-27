35109d83dc4a422d6b03b760348a16f165c3d680
Alexander <example@example.com>
2020-02-27 21:25:36 +0700

    Add initial state output

--- chess.c
+++ chess.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void print_board(char* board[]);
+
 int main(int argc, char* argv[])
 {
 	if (argc != 2) {
@@ -8,6 +10,29 @@ int main(int argc, char* argv[])
 	}
 	
 	char* filename = argv[1];
+
+	char* board[] = {"rnbqkbnr",
+					 "pppppppp",
+					 "        ",
+					 "        ",
+					 "        ",
+					 "        ",
+					 "PPPPPPPP",
+					 "RNBQKBNR"};
+
+	print_board(board);
 	
 	return 0;
+}
+
+void print_board(char* board[])
+{
+	for (int i = 0; i < 8; i++) {
+		printf("%d ", 8 - i);
+		for (int j = 0; j < 8; j++) {
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+	printf("  a b c d e f g h\n");
 }
