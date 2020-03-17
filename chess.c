498cec3be76c50145a9ad397c0d916095ac8b339
Крупенков Александр Евгеньевич <is942s08@awp05.csc.local>
2020-03-17 15:52:59 +0700

    Fix codestyle in chess.c

--- chess.c
+++ chess.c
@@ -4,35 +4,35 @@ void print_board(char* board[]);
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2) {
-		printf("Usage: ./chess [file]\n");
-		return 0;
-	}
-	
-	char* filename = argv[1];
+    if (argc != 2) {
+        printf("Usage: ./chess [file]\n");
+        return 0;
+    }
 
-	char* board[] = {"rnbqkbnr",
-					 "pppppppp",
-					 "        ",
-					 "        ",
-					 "        ",
-					 "        ",
-					 "PPPPPPPP",
-					 "RNBQKBNR"};
+    char* filename = argv[1];
 
-	print_board(board);
-	
-	return 0;
+    char* board[] = {"rnbqkbnr",
+                     "pppppppp",
+                     "        ",
+                     "        ",
+                     "        ",
+                     "        ",
+                     "PPPPPPPP",
+                     "RNBQKBNR"};
+
+    print_board(board);
+
+    return 0;
 }
 
 void print_board(char* board[])
 {
-	for (int i = 0; i < 8; i++) {
-		printf("%d ", 8 - i);
-		for (int j = 0; j < 8; j++) {
-			printf("%c ", board[i][j]);
-		}
-		printf("\n");
-	}
-	printf("  a b c d e f g h\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%d ", 8 - i);
+        for (int j = 0; j < 8; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("  a b c d e f g h\n");
 }
